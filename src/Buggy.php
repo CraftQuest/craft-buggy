7a8a93fd5c08611b2cfaf682b7c9934eea98f24d
Ryan Irelan <ryan@mijingo.com>
2021-06-28 17:33:12 -0500

    cleanup

--- src/Buggy.php
+++ src/Buggy.php
@@ -26,14 +26,6 @@ use craft\events\RegisterUrlRulesEvent;
 use yii\base\Event;
 
 /**
- * Craft plugins are very much like little applications in and of themselves. We’ve made
- * it as simple as we can, but the training wheels are off. A little prior knowledge is
- * going to be required to write a plugin.
- *
- * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL,
- * as well as some semi-advanced concepts like object-oriented programming and PHP namespaces.
- *
- * https://docs.craftcms.com/v3/extend/
  *
  * @author    Ryan Irelan
  * @package   Buggy
@@ -49,8 +41,6 @@ class Buggy extends Plugin
     // =========================================================================
 
     /**
-     * Static property that is an instance of this plugin class so that it can be accessed via
-     * Buggy::$plugin
      *
      * @var Buggy
      */
@@ -60,63 +50,37 @@ class Buggy extends Plugin
     // =========================================================================
 
     /**
-     * To execute your plugin’s migrations, you’ll need to increase its schema version.
-     *
      * @var string
      */
     public $schemaVersion = '1.0.0';
 
     /**
-     * Set to `true` if the plugin should have a settings view in the control panel.
      *
      * @var bool
      */
     public $hasCpSettings = false;
 
     /**
-     * Set to `true` if the plugin should have its own section (main nav item) in the control panel.
      *
      * @var bool
      */
-    public $hasCpSection = true;
+    public $hasCpSection = false;
 
     // Public Methods
     // =========================================================================
 
-    /**
-     * Set our $plugin static property to this class so that it can be accessed via
-     * Buggy::$plugin
-     *
-     * Called after the plugin class is instantiated; do any one-time initialization
-     * here such as hooks and events.
-     *
-     * If you have a '/vendor/autoload.php' file, it will be loaded for you automatically;
-     * you do not need to load it in your init() method.
-     *
-     */
     public function init()
     {
         parent::init();
         self::$plugin = $this;
 
-        // Add in our Twig extensions
-        Craft::$app->view->registerTwigExtension(new BuggyTwigExtension());
+//        Craft::$app->view->registerTwigExtension(new BuggyTwigExtension());
 
-        // Register our site routes
         Event::on(
             UrlManager::class,
             UrlManager::EVENT_REGISTER_SITE_URL_RULES,
             function (RegisterUrlRulesEvent $event) {
-                $event->rules['siteActionTrigger1'] = 'buggy/default';
-            }
-        );
-
-        // Register our CP routes
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['cpActionTrigger1'] = 'buggy/default/do-something';
+                $event->rules['siteActionTrigger1'] = 'buggy/default/create-swarm';
             }
         );
 
@@ -149,16 +113,6 @@ class Buggy extends Plugin
  * Craft::info(): record a message that conveys some useful information.
  * Craft::warning(): record a warning message that indicates something unexpected has happened.
  * Craft::error(): record a fatal error that should be investigated as soon as possible.
- *
- * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
- *
- * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
- * the category to the method (prefixed with the fully qualified class name) where the constant appears.
- *
- * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
- * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
- *
- * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
  */
         Craft::info(
             Craft::t(
@@ -167,35 +121,6 @@ class Buggy extends Plugin
                 ['name' => $this->name]
             ),
             __METHOD__
-        );
-    }
-
-    // Protected Methods
-    // =========================================================================
-
-    /**
-     * Creates and returns the model used to store the plugin’s settings.
-     *
-     * @return \craft\base\Model|null
-     */
-    protected function createSettingsModel()
-    {
-        return new Settings();
-    }
-
-    /**
-     * Returns the rendered settings HTML, which will be inserted into the content
-     * block on the settings page.
-     *
-     * @return string The rendered settings HTML
-     */
-    protected function settingsHtml(): string
-    {
-        return Craft::$app->view->renderTemplate(
-            'buggy/settings',
-            [
-                'settings' => $this->getSettings()
-            ]
         );
     }
 }
